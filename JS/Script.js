4b715b701f116aa0226e617ac9b6fb352a339f24
Vladyslav Marushevskyi <vladlisyt@gmail.com>
2026-06-22 23:30:28 +0300

    small correction

--- JS/Script.js
+++ JS/Script.js
@@ -1,7 +1,7 @@
 // console.log(typeof 12);
 // console.log(typeof "12");
 // console.log(typeof null);
-// console.log(typeof undefined);          << 3.1 DZ
+// console.log(typeof undefined);          //<< 3.1 DZ
 // console.log(typeof {});
 // console.log(typeof true);
 // console.log(typeof BigInt(12312));
@@ -9,15 +9,15 @@
 
 //=======================================================================
 
-// let age = prompt("Введіть Ваш вік:")
-// let Name = prompt("Введіть Ваше ім'я:")                    << 3.2 DZ
-// let City = prompt("Введіть Ваше місто:")
+// const age = prompt("Введіть Ваш вік:");
+// const Name = prompt("Введіть Ваше ім'я:");                    //<< 3.2 DZ
+// const City = prompt("Введіть Ваше місто:");
 
-// console.log(`Мене звати ${Name}, мені ${age} років. Проживаю в місті ${City}.`)
+// console.log(`Мене звати ${Name}, мені ${age} років. Проживаю в місті ${City}.`);
 
 
 //=======================================================================
 
-// const num = prompt("Введіть Ваше число:");                     << 3.3 DZ
+// const num = prompt("Введіть Ваше число:");                     //<< 3.3 DZ
                                                          
-// console.log(num.split("").join(" "))
+// console.log(num.split("").join(" "));
