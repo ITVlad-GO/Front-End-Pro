e17ce811590cc87a8446a8602d6ed0857b5509c7
Vladyslav Marushevskyi <vladlisyt@gmail.com>
2026-07-08 21:58:13 +0300

    add 7.2, 7,3  DZ

--- JS/script.js
+++ JS/script.js
@@ -15,3 +15,54 @@
 // const result = filterWords(arr2);
 
 // console.log(result.join(""));
+
+// =====================================================================================
+
+// const arr = [
+//   10,
+//   20,
+//   30,
+//   40,
+//   50,
+//   60,
+//   70,
+//   80,
+//   90,
+//   100,
+//   "Hello",
+//   true,                                              <<7.2 DZ
+//   false,
+//   null,
+//   undefined,
+//   { name: "Vlad" },
+//   [1, 2, 3],
+// ];
+
+// const sum = (arrForSum) => {
+//   const result = arrForSum.reduce((acc, item) => {
+//     if (typeof item === "number") {
+//       acc += item;
+//     }
+//     return acc;
+//   }, 0);
+//   return result;
+// };
+
+// const result = sum(arr);
+
+// console.log(result);
+
+// =====================================================================================
+
+// const array = [1, 3, 4, 6, 2, 5, 7];
+
+// const removeElement = (array, item) => {
+//   const index = array.indexOf(item);
+//   if(index !== -1) {
+//     array.splice(index, 1);                       <<7.3DZ
+//   }return array
+// };
+
+// const result = removeElement(array, 7)
+
+// console.log(result)
