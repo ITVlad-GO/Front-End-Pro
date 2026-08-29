a2f3557a74ac5c48ac11e6adacd42328fb1251f4
Vladyslav Marushevskyi <vladlisyt@gmail.com>
2026-08-29 19:29:19 +0300

    Dz13

--- DZ12.2/JS/script.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector(".buttons");
-
-btn.addEventListener("click", (event) => {
-  if (event.target.className === "btn1") {
-    alert(`Було натиснуто на : ${event.target.textContent}`);
-  }
-});
