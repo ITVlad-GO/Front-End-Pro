6ada8231b5684a57baf90cc3f1e2c5b09413bef8
Vladyslav Marushevskyi <vladlisyt@gmail.com>
2026-09-07 21:53:22 +0300

    Dz18

--- DZ18/JS/script.js
+++ DZ18/JS/script.js
@@ -1,17 +1,18 @@
-class Coach {
-    constructor(name, specialization, rating) {
-        this.name = name;
-        this.specialization = specialization;
-        this.rating = rating;
+let time = 84;
+
+const timer = document.querySelector("#timer");
+
+const interval = setInterval(() => {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    timer.textContent =
+        String(minutes).padStart(2, "0") + ":" +
+        String(seconds).padStart(2, "0");
+
+    time--;
+
+    if (time < 0) {
+        clearInterval(interval);
     }
-
-    displayInfo() {
-        console.log(`Coach: ${this.name}, Specialization: ${this.specialization}, Rating: ${this.rating}`);
-    }
-}
-
-const coach1 = new Coach('John Doe', 'Fitness', 4.7);
-const coach2 = new Coach('Alice Smith', 'Yoga', 4.9);
-
-coach1.displayInfo();
-coach2.displayInfo();
+}, 1000);
